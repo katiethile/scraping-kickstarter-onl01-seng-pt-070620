916cbea8446e6912e7119f9b669391e49328556f
Katie Le <lethikatie@gmail.com>
2020-08-11 02:08:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,3 +19,9 @@ create_project_hash
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
