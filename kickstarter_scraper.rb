f35cc24d1e36df588d1060806a8443f9e7888afb
Katie Le <lethikatie@gmail.com>
2020-08-11 02:13:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,23 +1,19 @@
 # file: kickstarter_scraper.rb
-
-require "nokogiri"
-require "pry"
-
-# projects: kickstarter.css("li.project.grid_4") 
-#This will select the first li with the project and grid_4 classes just so that we can make sure we've chosen our selectors correctly.
-
-
+ 
+require 'nokogiri'
+require 'pry'
+ 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
+ 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+ 
   projects = {}
  
   kickstarter.css("li.project.grid_4").each do |project|
@@ -29,4 +25,7 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
+ 
+  # return the projects hash
+  projects
 end
