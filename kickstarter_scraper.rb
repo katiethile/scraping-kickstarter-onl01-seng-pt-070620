c9987af08acb6fbe62fed0831fb4c291bbab012f
Katie Le <lethikatie@gmail.com>
2020-08-11 02:01:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,3 +11,6 @@ create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4") 
 #This will select the first li with the project and grid_4 classes just so that we can make sure we've chosen our selectors correctly.
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
