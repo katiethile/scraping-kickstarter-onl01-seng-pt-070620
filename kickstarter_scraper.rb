30b8986f52fb228a1e7019c2acb2659590f81a57
Katie Le <lethikatie@gmail.com>
2020-08-11 02:12:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,9 +14,10 @@ require "pry"
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
-
-
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
   projects = {}
  
   kickstarter.css("li.project.grid_4").each do |project|
