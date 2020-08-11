595230fe68adcadffe66cfcc725ee5dbbf7cb76c
Katie Le <lethikatie@gmail.com>
2020-08-11 01:55:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,4 +9,5 @@ end
  
 create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
+# projects: kickstarter.css("li.project.grid_4") 
+#This will select the first li with the project and grid_4 classes just so that we can make sure we've chosen our selectors correctly.
